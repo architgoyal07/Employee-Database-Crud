6d2ea905ba0bbff09d113b144d39e007a9c27cd2
architgoyal07 <architcool09@gmail.com>
2020-05-26 18:27:31 +0530

    Employee Database

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Employee Database</h1>
+    <employee-table v-bind:employee_list="employees"
+     @delete:employee="delEmployee" @edit:employee="editEmployee"/>
+    <EmployeeForm v-on:add:employee="addNewEmployee"/>
+   
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from './components/EmployeeTable'
+import EmployeeForm from '@/components/EmployeeForm'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+  EmployeeTable,
+  EmployeeForm
+  },
+  data(){
+    return{
+      employees:[
+        {
+          id:1,
+          name:'Archit Goyal',
+          email:'archit@gmail.com'
+        },
+        {
+          id:2,
+          name:'Pulkit Aggarwal',
+          email:'pulkit@gmail.com'
+        }
+      ],
+     
+    }
+  },
+  methods:{
+    addNewEmployee(newEmployee){
+    const lastId = this.employees.length>0? this.employees[this.employees.length-1].id:0;
+     const newEmployeeId=lastId+1;
+     const employee={...newEmployee,newEmployeeId};
+      this.employees=[...this.employees,employee];
+    
+      
+    },
+
+    delEmployee(empId){
+      
+      this.employees = this.employees.filter(employee => employee.id!==empId)
+     
+    },
+
+    editEmployee(editId,updatedEmployee){
+      this.employees = this.employees.map(employee =>
+        employee.id === editId ? updatedEmployee: employee
+      )
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style  scoped>
+.small-container{
+  max-width: 800px;
 }
-</style>
+
+ 
+</style>>
+
---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <form>
+            <label>Name</label>
+            <input type="text" v-model="employee.name">
+            <label>Email</label>
+            <input type="text" v-model="employee.email">
+            <button v-on:click="addEmployee">Add</button>
+        </form>
+    </div>
+</template>
+
+<script>
+//import {required,maxLength,minLength,between} from 'vuelidate/lib/validators'
+
+export default {
+
+    name:'EmployeeForm',
+    
+    data(){
+        return{
+            employee:{
+                
+                name:'',
+                email:''
+            }
+        }
+    },
+   /* validations:{
+                name:{
+                required,
+                minLength:minLength(3),
+                maxLength:maxLength(15)
+            },
+                email:{
+                    required,
+                    minLength:minLength(10),
+                    maxLength:maxLength(29)
+                }
+    },
+    */
+    methods:{
+        addEmployee(e){
+            e.preventDefault()
+            this.$emit('add:employee',this.employee)
+
+            this.employee.name ='';
+            this.employee.email ='';
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+</style>
